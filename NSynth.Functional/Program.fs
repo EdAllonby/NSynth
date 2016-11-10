51093f75609fe91102fbcc4164a0ad7773811f00
Ed Allonby <edward.allonby@gmail.com>
2016-11-10 19:05:00 +0000

    Add boilerplate for generating other types of waves

--- NSynth.Functional/Program.fs
+++ NSynth.Functional/Program.fs
@@ -2,24 +2,32 @@
 open NAudio.Dsp
 open System
 
-type sineWaveProvider(frequency, amplitude) = 
+type Wave = 
+    | Sine
+    | Triangle
+    | Square
+
+type sineWaveProvider(wave, frequency, amplitude) = 
     inherit WaveProvider32()
     override this.Read(buffer : float32 [], offset : int, sampleCount : int) = 
         let sampleRate = float this.WaveFormat.SampleRate
         let generateSine (sample : int) = 
             (amplitude * Math.Sin((2.0 * Math.PI * (float sample) * frequency) / sampleRate))
-
-        for i in 0..sampleCount do
-            buffer.[i + offset] <- float32 (generateSine i)
-
+        
+        let generateWave sample = 
+            match wave with
+            | Sine -> generateSine sample
+            | _ -> 1.0
+        for sample in 0..sampleCount do
+            buffer.[sample + offset] <- float32 (generateWave sample)
         sampleCount
 
 [<EntryPoint>]
 let main argv = 
-    let s = new sineWaveProvider(2000.0, 0.25)
+    let s = new sineWaveProvider(Wave.Sine, 500.0, 0.25)
     s.SetWaveFormat(44100, 1)
     let waveOut = new WaveOut()
     waveOut.Init(s)
     waveOut.Play()
     let wait = Console.ReadLine()
-    0 // return an integer exit code
+    0
